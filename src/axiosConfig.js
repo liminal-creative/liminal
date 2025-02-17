b219a61248669ae694442e1535cd0c39644c4776
steph-kimm <stephaniekim40@gmail.com>
2025-02-16 23:50:17 -0500

    added upload file component

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,16 @@
+// import axios from 'axios';
+
+// const axiosInstance = axios.create({
+//   baseURL: 'https://liminal-server.onrender.com', 
+// });
+
+// export default axiosInstance;
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://liminal-server.onrender.com', 
+  baseURL: window.location.hostname === 'localhost' 
+    ? 'http://localhost:5000'  // or whatever your local URL is
+    : 'https://liminal-server.onrender.com', 
 });
 
 export default axiosInstance;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FileUpload from '../components/FileUpload.js';
 
 const Home = () => {
   return (
